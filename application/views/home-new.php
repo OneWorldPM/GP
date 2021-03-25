846d6441f35f19de0c7c898ffa25983c673d2c05
Rexter Dayuta <rexterdayuta@gmail.com>
2021-03-26 01:43:10 +0800

    Main landing page/Lobby update

--- application/views/home-new.php
+++ application/views/home-new.php
@@ -10,10 +10,11 @@
     <title>Gravity Productions</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?=base_url()?>front_assets/login_template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>front_assets/login_template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="<?=base_url()?>front_assets/login_template/vendor/bootstrap/css/narrow-jumbotron.css" rel="stylesheet">
+    <link href="<?= base_url() ?>front_assets/login_template/vendor/bootstrap/css/narrow-jumbotron.css"
+          rel="stylesheet">
 </head>
 
 <style>
@@ -28,50 +29,50 @@
     }
 </style>
 
-<body style="background-image: url(<?=base_url()?>front_assets/gp/668940634.jpg);background-size: cover;background-repeat: no-repeat;">
+<body style="background-image: url(<?= base_url() ?>front_assets/gp/668940634.jpg);background-size: cover;background-repeat: no-repeat;">
 
 <div class="container">
     <div class="jumbotron" style="background-color: #002f7000;">
         <video autoplay muted loop class="gpLogoAnimated" style="width: 100%; height: auto;">
-            <source src="<?=base_url()?>front_assets/gp/Animated_GP_Logo_Cropped-low_size.mp4" type="video/mp4">
+            <source src="<?= base_url() ?>front_assets/gp/Animated_GP_Logo_Cropped-low_size.mp4" type="video/mp4">
         </video>
     </div>
-
-    <div class="row text-center">
-        <div class="col-md-4">
-            <div style="border: 1px solid white;border-radius: 25px;padding: 10px;margin-bottom: 10px;cursor: pointer;background-color: #ffffff30;" onclick="location.href='<?= base_url() ?>sessions'">
-                <i class="fas fa-chalkboard-teacher" style="font-size: 125px !important; color: #009ce9;"></i>
-                <div style="margin-top: 15px;color: white;font-size: 25px;font-weight: bold;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;">SESSIONS</div>
+    <div style="margin:auto !important">
+        <div class="row text-center">
+            <div class="col-md-2">
             </div>
-        </div>
-        <div class="col-md-4">
-            <div style="border: 1px solid white;border-radius: 25px;padding: 10px;margin-bottom: 10px;cursor: pointer;background-color: #ffffff30;" onclick="location.href='<?= base_url() ?>lounge'">
-                <i class="fas fa-couch" style="font-size: 125px !important; color: #009ce9;"></i>
-                <div style="margin-top: 15px;color: white;font-size: 25px;font-weight: bold;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;">LOUNGE</div>
+            <div class="col-md-2">
+                <div style="background-color: #002F70; cursor: pointer; border-radius: 25px" onclick="location.href='<?= base_url() ?>sessions'">
+                    <i class="fas fa-chalkboard-teacher"
+                       style="font-size: 125px !important; color: white; margin-top:10px;"></i>
+                    <div style="margin-top: 15px;color: white;font-size: 25px;font-weight: bold;">SESSIONS</div>
+                </div>
             </div>
-        </div>
-        <div class="col-md-4">
-            <div style="border: 1px solid white;border-radius: 25px;padding: 10px;margin-bottom: 10px;cursor: pointer;background-color: #ffffff30;" onclick="location.href='<?= base_url() ?>sponsor'">
-                <i class="fas fa-compress-arrows-alt" style="font-size: 125px !important; color: #009ce9;"></i>
-                <div style="margin-top: 15px;color: white;font-size: 25px;font-weight: bold;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;">EXHIBITS</div>
+            <div class="col-md-2">
+                <div style="background-color: #002F70; cursor: pointer; border-radius: 25px" onclick="location.href='<?= base_url() ?>lounge'">
+                    <i class="fas fa-couch" style="font-size: 135px !important; color: white; "></i>
+                    <div style="margin-top: 15px;color: white;font-size: 25px;font-weight: bold;">LOUNGE</div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div style="background-color: #002F70; cursor: pointer; border-radius: 25px" onclick="location.href='<?= base_url() ?>sponsor'">
+                    <i class="fas fa-compress-arrows-alt"
+                       style="font-size: 125px !important; color: white; margin-top:10px;"></i>
+                    <div style="margin-top: 15px;color: white;font-size: 25px;font-weight: bold;">EXHIBITS</div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div style="background-color: #002F70; cursor: pointer; border-radius: 25px" onclick="location.href='/support'">
+                    <i class="fas fa-cog" style="font-size: 105px !important; color: white; margin-top:10px;"></i>
+                    <div style="margin-top: 15px;color: white;font-size: 20px;font-weight: bold;">TECHNICAL SUPPORT
+                    </div>
+                </div>
+                <div class="col-md-2">
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="row text-center">
-        <div class="col-md-6 text-right">
-            <div class="text-center" style="border: 1px solid white;border-radius: 25px;padding: 10px;margin-bottom:5px;width: 240px;display: inline-block;cursor: pointer;background-color: #ffffff30;">
-                <i class="fas fa-info-circle" style="font-size: 95px !important; color: #009ce9;"></i>
-                <div style="margin-top: 15px;color: white;font-size: 20px;font-weight: bold;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;">HELP DESK</div>
-            </div>
-        </div>
-        <div class="col-md-6 text-left">
-            <div class="text-center" style="border: 1px solid white;border-radius: 25px;padding: 10px;margin-bottom:5px;width: 240px;display: inline-block;cursor: pointer;background-color: #ffffff30;" onclick="location.href='/support'">
-                <i class="fas fa-cog" style="font-size: 95px !important; color: #009ce9;"></i>
-                <div style="margin-top: 15px;color: white;font-size: 20px;font-weight: bold;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;">TECHNICAL SUPPORT</div>
-            </div>
-        </div>
-    </div>
 
 </div> <!-- /container -->
 
@@ -79,7 +80,7 @@
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="<?=base_url()?>front_assets/login_template/vendor/bootstrap/js/ie10-viewport-bug-workaround.js"></script>
+<script src="<?= base_url() ?>front_assets/login_template/vendor/bootstrap/js/ie10-viewport-bug-workaround.js"></script>
 <script src="https://kit.fontawesome.com/fd91b3535c.js" crossorigin="anonymous"></script>
 </body>
 </html>
